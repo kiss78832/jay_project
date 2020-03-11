6169e12efd0c55df74ef64eb0b0d8280fa2beb36
kiss78832 <59165393+kiss78832@users.noreply.github.com>
2020-03-11 17:34:49 +0800

    temp save

--- Jay_workspace/Java8_Example/src/Optional_Example/Optional01.java
+++ Jay_workspace/Java8_Example/src/Optional_Example/Optional01.java
@@ -1,5 +1,16 @@
 package Optional_Example;
 
 public class Optional01 {
-
+	/*
+	 * Optional.of(T t): 創建一個Optional 實體。
+	 * Optional.empty(): 創建一個空的Optional 實體。
+	 * Optional.ofNullable(T t): 若 t 不為null，創建Optional 實體，否則創建空實體。
+	 * isPresnt(): 判斷是否包含值。
+	 * orElse(T t): 如果調用對象包含值，返回該值，否則返回t。
+	 * orElseGet(Suppplier s): 如果調用對象包含值，返回該值，否則返回s或取的值。
+	 * map(Function f): 如果值對其處理，並返回處理後的Optional，否則返回Optional.empty()。
+	 * flatMap(Function mapper): 與map類似，要求返回值必須是Optional。
+	 * 
+	 * 進度:15章節剛開始
+	 */
 }
