fdf06c5fd0fec7490151dab331a933ffba8ba53d
kiss78832 <59165393+kiss78832@users.noreply.github.com>
2020-03-12 17:33:33 +0800

    add Date01 part1 finish

--- Jay_workspace/Java8_Example/src/Date_Example/Date01.java
+++ Jay_workspace/Java8_Example/src/Date_Example/Date01.java
@@ -1,6 +1,12 @@
 package Date_Example;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.Period;
+import java.time.ZoneOffset;
 
 import org.junit.Test;
 
@@ -10,9 +16,10 @@ import org.junit.Test;
 public class Date01 {
 
 	/*
-	 * (1).LocalDate、LocalTime、LocalDateTime 所創的實體都是「不可變」，採用ISO-8601的國際日期標準。	test01()
-	 * (2).Instant : 時間戳記(以Unix元年: 1970年1月1日00:00:00到某時間的毫秒值) 					   	test02()
-	 * 
+	 * (1).LocalDate、LocalTime、LocalDateTime 所創的實體都是「不可變」，採用ISO-8601的國際標準化組織。	test01()
+	 * (2).Instant: 時間戳記(以Unix元年: 1970年1月1日00:00:00到某時間的毫秒值) 					   	test02()
+	 * (3).Duration: 計算兩個 " 時間 " 之間的區隔													test03()
+	 * (4).Period: 計算兩個 " 日期 " 之間的區隔													test04()
 	 */
 	@Test
 	public void test01() {
@@ -43,6 +50,57 @@ public class Date01 {
 		System.out.println(dateTime.getMonth());//	英文
 		System.out.println(dateTime.getMonthValue());//	中文
 		System.out.println(dateTime.getDayOfWeek());//	禮拜幾(英文)
+	}
+	
+	@Test
+	public void test02() {
+		//	Instant.now()   默認獲取UTC時區，格林威治標準時間。
+		Instant instant = Instant.now();
+		System.out.println("格林威治標準時間: "+instant);
 		
+		System.out.println("----------------------------");
+		
+		//	instant.atOffset()  台北時區+8:00
+		OffsetDateTime taipeiTime = instant.atOffset(ZoneOffset.ofHours(8));
+		System.out.println("taipeiTime: "+taipeiTime);
+		
+		System.out.println("----------------------------");
+		
+		//	instant.toEpochMilli()   時間轉成毫秒
+		System.out.println("毫秒: "+instant.toEpochMilli());
+		
+		System.out.println("----------------------------");
+		
+		//	Instant.ofEpochSecond()    1970-01-01 00:00:00 + 秒
+		Instant SecondChangDate = Instant.ofEpochSecond(60);
+		System.out.println(SecondChangDate);
+	}
+	
+	@Test
+	public void test03() throws InterruptedException {
+		//	也能使用LocalDateTime系列去計算
+		Instant start = Instant.now();
+		
+		Thread.sleep(5000); //	睡5000毫秒
+		
+		Instant end = Instant.now();
+		
+		//getXXX裡頭只有秒、toXXX裡頭有毫秒、分鐘、時等等...
+		Duration duration = Duration.between(start, end);
+		System.out.println("ISO格式: "+duration);
+		System.out.println("----------------------");
+		System.out.println("Start 比較 End 相差: "+duration.toMillis()+"豪秒");
+	}
+	
+	@Test
+	public void test04() {
+		LocalDate date01 = LocalDate.of(2015, 01, 01);
+		LocalDate date02 = LocalDate.now();
+		
+		//	兩個日期互相比較
+		Period period = Period.between(date01, date02);
+		System.out.println("date01 與 date02相差幾年: "+period.getYears());
+		System.out.println("date01 與 date02相差幾月: "+period.getMonths());
+		System.out.println("date01 與 date02相差幾日: "+period.getDays());
 	}
 }
