4cddb8de2b1de87daa6c1be22ae7167af561d5ad
kiss78832 <59165393+kiss78832@users.noreply.github.com>
2020-04-15 13:59:38 +0800

    springmvc結尾

--- Jay_workspace/SpringMVC_Process/src/com/jay/springmvc/HelloWorld.java
+++ Jay_workspace/SpringMVC_Process/src/com/jay/springmvc/HelloWorld.java
@@ -1,10 +1,18 @@
 package com.jay.springmvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloWorld {
+	
+	@Autowired
+	private UserService userService;
+	
+	public HelloWorld() {
+		System.out.println("初始化，創建HelloWorld建構子");
+	}
 	
 	@RequestMapping("/helloworld")
 	public String hello() {
---
+++ Jay_workspace/SpringMVC_Process/src/com/jay/springmvc/UserService.java
@@ -0,0 +1,11 @@
+package com.jay.springmvc;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+	
+	public UserService() {
+		System.out.println("初始化，創建UserService建構子");
+	}
+}
